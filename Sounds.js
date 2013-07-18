5fa26040ddcf03bb8bda6fbd8d600337c80163d9
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-07-18 13:35:27 +0200

    UMD pattern updated

--- Sounds.js
+++ Sounds.js
@@ -1,5 +1,7 @@
-// AMD + global
+// AMD + Global: r.js compatible
+// Use START + END markers to keep module content only
 (function(root,define){ define([], function() {
+// START: Module logic start
 
 	// HTML5 Sounds manager
 	function Sounds(folder,loadCallback) {
@@ -115,13 +117,16 @@
 		this.muted=muted;
 	};
 
+// END: Module logic end
+
 	return Sounds;
 
-})})(this,typeof define === 'function' && define.amd ? define : function (name, deps, factory) {
-	if(typeof name === 'Object') {
-		deps=name; factory=deps;
+});})(this,typeof define === 'function' && define.amd ? define : function (name, deps, factory) {
+	var root=this;
+	if(typeof name === 'object') {
+		factory=deps; deps=name; name='Sounds';
 	}
-	this['Sounds']=factory.apply(this, deps.map(function(){
-		return this[deps.substring(deps.lastIndexOf('/')+1)];
+	this[name.substring(name.lastIndexOf('/')+1)]=factory.apply(this, deps.map(function(dep){
+		return root[dep.substring(dep.lastIndexOf('/')+1)];
 	}));
-});
+}.bind(this));
